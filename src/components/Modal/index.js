84e278d64263d62399dd9b6c95d097d8f82b17de
yolkpie2020 <yolkpie2020@163.com>
2021-03-20 09:15:15 +0000

    travis-ci auto build components-vue

--- src/components/Modal/index.js
+++
@@ -1,8 +0,0 @@
-import Modal from './Modal.vue'
-import packageComponent from '../utils/package'
-import Vue from 'vue'
-import { Button } from 'vant';
-
-Vue.use(Button)
-
-export default packageComponent(Modal)
